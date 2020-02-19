b27a540c08cae677eb858d82e68ebb66ad499d31
pavel <pavel.pronchatov155@gmail.com>
2020-02-19 23:15:28 +0400

    feat: typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if( array == undefined || array.length == 0 ){
+    return 0;
+  }
+
+  let res = array[0];
+  for(let i = 0; i < array.length; i++){
+    if (res > array[i]){
+      res = array[i];
+    }
+  }
+  return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if( array == undefined || array.length == 0 ){
+    return 0;
+  }
+
+  let res = array[0];
+  for(let i = 0; i < array.length; i++){
+    if (res < array[i]){
+      res = array[i];
+    }
+  }
+  return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if( array == undefined || array.length == 0 ){
+    return 0;
+  }
+
+  let res = 0;
+  for(let i = 0; i < array.length; i++){
+   res += array[i];
+   
+  }
+  return res/(array.length);
 }
+
+
